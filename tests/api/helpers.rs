15c7c1f844bd6cf9d584e5c1daaa9ee4bd773e00
jimmycarrlin <82116903+jimmycarrlin@users.noreply.github.com>
2022-12-12 17:53:43 +0300

    Adjust post_newsletters method

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -49,7 +49,7 @@ impl TestApp {
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
         reqwest::Client::new()
-            .post(&format!("{}/newsletters", &app.address))
+            .post(&format!("{}/newsletters", &self.address))
             .json(&body)
             .send()
             .await
